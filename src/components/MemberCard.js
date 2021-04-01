0c6d550f11b8d1a9bcfff2302888b1fe6313ae1e
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-04-01 15:39:47 -0700

    task5 completed

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -1,9 +1,34 @@
-const MemberCard = () => {
-
-
+import { Link } from 'react-router-dom';
+const MemberCard = ( {member} ) => {
     return (
-        <div></div>
+        <div>
+        <div className="card mb-3 text-center">
+            <div className="card-header text-center">{member.bio}</div>
+            <div className="th-card-bg-img mt-3" style={{'backgroundImage': `url(${member.profile_img})`}}></div>
+            <div className="card-body">
+                <h4 className='card-title'>
+                    {member.firstName} {member.lastName}
+                </h4>
+                <h6 className="text-secondary">{member.alter_ego}</h6>
+                <div className="my-2">
+                    <strong>Email: </strong>
+                    {member.email}
+                </div>
+                <div className="my-2">
+                    <strong>
+                        Languages: 
+                    </strong>
+                    {member.languages?.map((language, i) => {
+                        return <small key={i}> {language}{i === member.languages.length - 1 ? '' : ', ' } </small>
+                    })}
+                </div>
+                <div className="my-2 d-flex justify-content-center text-center">
+                    <Link to={`/members/${member.id}`} className="card-link">View Details
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </div>
     );
 }
-
 export default MemberCard;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,16 +1,55 @@
-// import { useEffect, useState } from 'react';
-// import MemberCard from '../components/MemberCard'
-// import { memberData } from '../data/members';
-// import '../App.css';
+import { useEffect, useState } from 'react';
+import MemberCard  from '../components/MemberCard';
+import { membersData } from '../data/membersData';
+import '../App.css';
+
 
 const Dashboard = () => {
+    const [members, setMembers] = useState ([]);
+    const [alert, setAlert] = useState(false);
+
+    useEffect(() => {
+        let featured = membersData.filter(member => member.featured);
+        setMembers(featured);
+    }, [alert]);
+
+    const updatedFeatured = memberId => {
+        let foundMember = membersData.find(member => member.id ===+memberId);
+        foundMember.featured = !foundMember.featured;
+        showAlert();
+    };
+
+    const showAlert = () => {
+        setAlert(true);
+        setTimeout(() => {
+            setAlert(false);
+        },);
+    };
 
 
     return (
-        <div>
-            <h2>This is my Dashboard Page!</h2>
+        <div id='dashboard-page'>
+            <div className='row text-center mt-3'>
+                <div className='col'>
+                    <h2>Team Alkali</h2>
+            <h4 className='text-secondary'>Who We Are!</h4>
+            
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit eius iusto accusamus culpa laudantium, officiis harum voluptatibus, ipsa et repellendus qui, iure minus quisquam odio maiores aliquid facilis repudiandae! Consequuntur ipsum eaque culpa porro sunt, cum distinctio est atque omnis animi eos quisquam deleniti voluptate rerum velit inventore rem facere!</p>
+
+            </div>
+        </div>
+        
+        <div className='row'>
+            {members.map((member, index) => {
+                return (
+                    <div className='col' key={member.id}>
+                    <MemberCard member={member} updatedFeatured={updatedFeatured} />
+                    </div>
+                );
+            })}
+        </div>
         </div>
     );
-}
+};
 
 export default Dashboard;
